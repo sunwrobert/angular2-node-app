7194d06fcc63c9f4c8b98e446b642451bb61e0cf
Robert Sun <sunwrobert@gmail.com>
2016-11-22 16:44:26 -0500

    initial commit

---
+++ routes/app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res, next) {
+    res.render('index');
+});
+
+router.get('/test', function(req, res, next){
+    res.render('test');
+});
+
+module.exports = router;
