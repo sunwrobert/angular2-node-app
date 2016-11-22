cd291d21371b5c3d4fa6db5952a22d9be5bc1a66
Robert Sun <sunwrobert@gmail.com>
2016-11-22 18:01:00 -0500

    add form to add user demo

--- routes/app.js
+++ routes/app.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    res.render('index');
+    User.findOne({}, function(err, doc){
+        if(err){
+            return res.send('error');
+        }
+        res.render('node', {email: doc.email});        
+    });
 });
-
-router.get('/test', function(req, res, next){
-    res.render('test');
+router.post('/', function(req, res, next){
+    var email = req.body.email;
+    var user = new User({
+        firstName: 'Robert',
+        lastName: 'Sun',
+        password: 'test',
+        email: email
+    });
+    user.save();
+    res.redirect('/');
 });
 
 module.exports = router;
